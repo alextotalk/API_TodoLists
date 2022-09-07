5258a07601d1499d954db563e573f925830a5df9
alextotalk <alexoo@inbox.ru>
2022-09-07 20:17:03 +0300

    For summary

---
+++ pkg/handler/auth_test.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"bytes"
+	"errors"
+	todo "github.com/alextotalk/API_TodoLists"
+	"github.com/alextotalk/API_TodoLists/pkg/service"
+	serviceMocks "github.com/alextotalk/API_TodoLists/pkg/service/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/magiconair/properties/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_signUp(t *testing.T) {
+	// Init Test Table
+	type mockBehavior func(r *serviceMocks.MockAuthorization, user todo.User)
+
+	tests := []struct {
+		name                 string
+		inputBody            string
+		inputUser            todo.User
+		mockBehavior         mockBehavior
+		expectedStatusCode   int
+		expectedResponseBody string
+	}{
+		{
+			name:      "Ok",
+			inputBody: `{"username": "username", "name": "Test Name", "password": "qwerty"}`,
+			inputUser: todo.User{
+				Username: "username",
+				Name:     "Test Name",
+				Password: "qwerty",
+			},
+			mockBehavior: func(r *serviceMocks.MockAuthorization, user todo.User) {
+				r.EXPECT().CreateUser(user).Return(1, nil)
+			},
+			expectedStatusCode:   200,
+			expectedResponseBody: `{"id":1}`,
+		},
+		{
+			name:                 "Wrong Input",
+			inputBody:            `{"username": "username"}`,
+			inputUser:            todo.User{},
+			mockBehavior:         func(r *serviceMocks.MockAuthorization, user todo.User) {},
+			expectedStatusCode:   400,
+			expectedResponseBody: `{"message":"invalid input body"}`,
+		},
+		{
+			name:      "Service Error",
+			inputBody: `{"username": "username", "name": "Test Name", "password": "qwerty"}`,
+			inputUser: todo.User{
+				Username: "username",
+				Name:     "Test Name",
+				Password: "qwerty",
+			},
+			mockBehavior: func(r *serviceMocks.MockAuthorization, user todo.User) {
+				r.EXPECT().CreateUser(user).Return(0, errors.New("something went wrong"))
+			},
+			expectedStatusCode:   500,
+			expectedResponseBody: `{"message":"something went wrong"}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Init Dependencies
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			repo := serviceMocks.NewMockAuthorization(c)
+			test.mockBehavior(repo, test.inputUser)
+
+			services := &service.Service{Authorization: repo}
+			handler := Handler{services}
+
+			// Init Endpoint
+			r := gin.New()
+			r.POST("/sign-up", handler.signUp)
+
+			// Create Request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/sign-up",
+				bytes.NewBufferString(test.inputBody))
+
+			// Make Request
+			r.ServeHTTP(w, req)
+
+			// Assert
+			assert.Equal(t, w.Code, test.expectedStatusCode)
+			assert.Equal(t, w.Body.String(), test.expectedResponseBody)
+		})
+	}
+}
---
+++ pkg/handler/handler.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	_ "github.com/alextotalk/API_TodoLists/docs"
+	"github.com/alextotalk/API_TodoLists/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	//router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	api := router.Group("/api", h.userIdentity)
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+			}
+		}
+
+		items := api.Group("items")
+		{
+			items.GET("/:id", h.getItemById)
+			items.PUT("/:id", h.updateItem)
+			items.DELETE("/:id", h.deleteItem)
+		}
+	}
+
+	return router
+}
